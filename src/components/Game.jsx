cf7c2e0bac7a24cabda255632a0f156c73b87219
RobFrontend <robert.grabowski97@gmail.com>
2025-09-24 17:15:45 +0200

    bonusy

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,8 +8,17 @@ function Game({ isOpen }) {
   const playerRef = useRef(null);
   const textTestRef = useRef(null);
   const { data } = useArticles();
+  const [isJumpPower, setIsJumpPower] = useState(600);
+  const [isPlayerSpeed, setIsPlayerSpeed] = useState(150);
 
   // to let Kaplay be inicialized only one time (there was a bug withour that)
+  const jumpPowerRef = useRef(isJumpPower);
+  const playerSpeedRef = useRef(isPlayerSpeed);
+
+  useEffect(() => {
+    jumpPowerRef.current = isJumpPower;
+    playerSpeedRef.current = isPlayerSpeed;
+  }, [isJumpPower, isPlayerSpeed]);
   useEffect(() => {
     if (!canvasRef.current || kaplayRef.current || !data) return;
 
@@ -53,17 +62,58 @@ function Game({ isOpen }) {
       k.body({ isStatic: true }),
     ]);
 
-    // obstacle
+    // obstacles with bonuses
     setTimeout(() => {
-      k.add([
+      const boxjump = k.add([
         k.rect(60, 60),
         k.pos(500, 500),
         k.color(125, 125, 0),
         k.area(),
         k.body(),
+        k.anchor("center"),
         "orangebox",
       ]);
+
+      boxjump.add([
+        k.text("+jump 5sec", { size: 16 }),
+        k.color(255, 255, 255),
+        k.anchor("center"),
+      ]);
     }, 2000);
+
+    setTimeout(() => {
+      const boxspeed = k.add([
+        k.rect(60, 60),
+        k.pos(200, 200),
+        k.color(244, 244, 244),
+        k.area(),
+        k.body(),
+        k.anchor("center"),
+        "whitebox",
+      ]);
+      boxspeed.add([
+        k.text("+speed 5sec", { size: 16 }),
+        k.color(186, 0, 0),
+        k.anchor("center"),
+      ]);
+    }, 4000);
+
+    setTimeout(() => {
+      const boxsize = k.add([
+        k.rect(60, 60),
+        k.pos(900, 400),
+        k.color(186, 0, 0),
+        k.area(),
+        k.body({ isStatic: true }),
+        k.anchor("center"),
+        "redbox",
+      ]);
+      boxsize.add([
+        k.text("+size 10sec", { size: 16 }),
+        k.color(255, 255, 255),
+        k.anchor("center"),
+      ]);
+    }, 3000);
 
     // fullscreen button
     const btn1 = k.add([k.rect(10, 10), k.pos(1180, 10), k.area(), "btn1"]);
@@ -90,8 +140,9 @@ function Game({ isOpen }) {
       k.body(),
       k.health(100),
       k.pos(600, 600),
+      k.scale(1),
       "player",
-      { dead: false, speed: 150 },
+      { dead: false, speed: playerSpeedRef.current },
     ]);
     playerRef.current = player;
 
@@ -100,8 +151,24 @@ function Game({ isOpen }) {
     player.onCollide("orangebox", () => {
       player.hurt(20);
       if (data[1]) textTest.text = data[1].title;
+      setIsJumpPower(1200);
+      setTimeout(() => {
+        setIsJumpPower(600);
+      }, 5000);
     });
-  }, [data]);
+    player.onCollide("whitebox", () => {
+      setIsPlayerSpeed(450);
+      setTimeout(() => {
+        setIsPlayerSpeed(150);
+      }, 5000);
+    });
+    player.onCollide("redbox", () => {
+      player.scaleTo(2);
+      setTimeout(() => {
+        player.scaleTo(1);
+      }, 2500);
+    });
+  }, [data, isJumpPower, isPlayerSpeed]);
 
   // 2. Controls
   useEffect(() => {
@@ -110,9 +177,19 @@ function Game({ isOpen }) {
     if (!k || !player) return;
 
     if (!isOpen) {
-      k.onKeyDown("right", () => !player.dead && player.move(player.speed, 0));
-      k.onKeyDown("left", () => !player.dead && player.move(-player.speed, 0));
-      k.onKeyDown("space", () => player.isGrounded() && player.jump(600));
+      k.onKeyDown(
+        "right",
+        () => !player.dead && player.move(playerSpeedRef.current, 0)
+      );
+      k.onKeyDown(
+        "left",
+        () => !player.dead && player.move(-playerSpeedRef.current, 0)
+      );
+      k.onKeyDown("space", () => {
+        if (player.isGrounded()) {
+          player.jump(jumpPowerRef.current);
+        }
+      });
     }
   }, [isOpen]);
 
